14fb83ad1e888cd6e6139d98128bf1ebd6b00328
kiseop lee <kiseop.lee@gmail.com>
2018-03-03 10:34:34 +0900

    docker를 이용한 node, express 초반 작업 완료

---
+++ docker/start.sh
@@ -0,0 +1,6 @@
+sleep 2
+cd /usr/src/app
+
+npm link express
+
+express my-app
--- start.bat
+++ start.bat
@@ -3,4 +3,4 @@ set CURPATH=%cd%\www
 docker build --build-arg SERVICE_STATE=dev -t vuejs ./docker
 docker kill vuejs
 docker rm vuejs
-docker run --rm --name vuejs -p 8000:3000 vuejs
+docker run --rm --name vuejs -p 8000:8081 -v %CURPATH%:/usr/src/app vuejs
